a159e3eab9669c80bd04d404a0e32b26b330de11
Tanya2376 <asdrewq.99@mail.ru>
2022-05-19 13:04:04 +0300

    init repos

---
+++ Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Console_calc
+{
+    public class Math
+    {
+        public double MathResult(double a, double b, string oper)
+        {
+            double result;
+            switch (oper)
+            {
+                case "+":
+                    result = a + b;
+                    return result;
+                case "-":
+                    result = a - b;
+                    return result;
+                case "*":
+                    result = a * b;
+                    return result;
+                case "/":
+                    result = a / b;
+                    return result;
+                default:
+                    Console.WriteLine("неверный оператор");
+                    return 0;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Console_calc
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            string input2 = "";
+
+            Console.WriteLine("Calculator");
+            while (true)
+            {
+                
+                Console.WriteLine("Введите первое число (может быть целым, либо дробным с разделителем ','), затем Enter");
+                var input1 = Console.ReadLine();
+
+                if (!Double.TryParse(input1, out a))
+                {
+                    Console.WriteLine("неверное число, попробуйте снова");
+                    continue;
+                }
+
+                Console.WriteLine("Введите математический оператор( + - * / ), затем Enter");
+                input2 = Console.ReadLine();
+                switch (input2)
+                {
+                    case "+":
+                        break;
+                    case "-":
+                        break;
+                    case "*":
+                        break;
+                    case "/":
+                        break;
+                    default:
+                        Console.WriteLine("неверный оператор");
+                        continue;
+                }
+
+                Console.WriteLine("Calculator");
+                Console.WriteLine("Введите второе число (может быть целым, либо дробным с разделителем ','), затем Enter");
+                var input3 = Console.ReadLine();
+
+                if (!Double.TryParse(input3, out b))
+                {
+                    Console.WriteLine("неверное число, попробуйте снова");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+
+            Math math = new Math();
+            
+            Console.WriteLine( a + " " + input2 + " " + b + " = " + math.MathResult(a, b, input2));
+            Console.ReadLine();
+        }
+    }
+}
