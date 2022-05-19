2ed15607e9c86f2f034f55b83fcce8edfef9759e
Tanya2376 <asdrewq.99@mail.ru>
2022-05-19 14:09:12 +0300

    Проверка на выход из программы

--- Program.cs
+++ Program.cs
@@ -4,17 +4,30 @@ namespace Console_calc
 {
     class Program
     {
-        
+
         static void Main(string[] args)
+        {
+
+            Console.WriteLine("Добро пожаловать в супер-наикрутейший калькулятор 21 века!");
+            Console.WriteLine("Calculator");
+
+            do
+            {
+                Cycle();
+                Console.WriteLine("Завершить работу программу (Y / N)?");
+            }
+            while (Console.ReadLine() == "N");
+        }
+
+        static void Cycle()
         {
             double a;
             double b;
             string input2 = "";
-            Console.WriteLine("Добро пожаловать в супер-наикрутейший калькулятор 21 века!");
-            Console.WriteLine("Calculator");
+
             while (true)
             {
-                
+
                 Console.WriteLine("Введите первое число (может быть целым, либо дробным с разделителем ','), затем Enter");
                 var input1 = Console.ReadLine();
 
@@ -41,7 +54,7 @@ namespace Console_calc
                         continue;
                 }
 
-                Console.WriteLine("Calculator");
+                
                 Console.WriteLine("Введите второе число (может быть целым, либо дробным с разделителем ','), затем Enter");
                 var input3 = Console.ReadLine();
 
@@ -55,12 +68,15 @@ namespace Console_calc
                     break;
                 }
             }
-            
+
 
             Math math = new Math();
-            
-            Console.WriteLine( a + " " + input2 + " " + b + " = " + math.MathResult(a, b, input2));
-            Console.ReadLine();
+
+            Console.WriteLine(a + " " + input2 + " " + b + " = " + math.MathResult(a, b, input2));
+           
         }
+
     }
+
+
 }
